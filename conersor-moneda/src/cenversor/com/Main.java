93a9a68c7b0044103483612956a0b75d82764b2a
marlon <mj.moreno.developer@gmail.com>
2023-03-14 19:27:22 -0300

    conversion para varias convinaciones de moneda

---
+++ conersor-moneda/src/cenversor/com/Main.java
@@ -0,0 +1,11 @@
+package cenversor.com;
+
+public class Main {
+  public static void main(String[] args) {
+	  
+}
+  
+  public static void convertir(double) {
+	
+}
+}
--- conersor-moneda/src/interfaz/Gui.java
+++ conersor-moneda/src/interfaz/Gui.java
@@ -1,21 +1,21 @@
 package interfaz;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.SwingConstants;
-import java.awt.Font;
-import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JSeparator;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class Gui extends JFrame {
 
@@ -24,7 +24,12 @@ public class Gui extends JFrame {
 	private JLabel lblTitulo;
 	private JLabel lblCoonversor1;
 	private JLabel lblConversor2;
-	private JTextField textField;
+	
+	double cantidad;
+	double conversion;
+	String de = "ARG";
+	String a = "ARG";
+	
 
 	/**
 	 * Launch the application.
@@ -47,14 +52,14 @@ public class Gui extends JFrame {
 	 */
 	public Gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 372, 262);
+		setBounds(100, 100, 372, 220);
 		contenedorPrincipal = new JPanel();
 		contenedorPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
 		iniciarComponente();
 		
 		setTitle("CONVERSOR MONEDA");
-		//setResizable(false);
+		setResizable(false);
 		setLocationRelativeTo(null);
 		
 
@@ -66,13 +71,26 @@ public class Gui extends JFrame {
 		setContentPane(contenedorPrincipal);
 		contenedorPrincipal.setLayout(null);
 		
-		JComboBox comboMoneda1 = new JComboBox();
-		comboMoneda1.setModel(new DefaultComboBoxModel(new String[] {"ARG", "USD", "EUR", "YEN", "LIBRAS", "WON"}));
+		JComboBox<Object> comboMoneda1 = new JComboBox<Object>();
+		comboMoneda1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				de = comboMoneda1.getSelectedItem().toString();
+				//convertir();
+			}
+		});
+		comboMoneda1.setModel(new DefaultComboBoxModel(new String[] {"ARS", "USD", "EUR", "YEN", "LIBRAS", "WON"}));
 		comboMoneda1.setBounds(159, 64, 61, 22);
 		contenedorPrincipal.add(comboMoneda1);
 		
-		JComboBox comboMoneda2 = new JComboBox();
-		comboMoneda2.setModel(new DefaultComboBoxModel(new String[] {"ARG", "USD", "EUR", "LIBRAS", "YEN", "WON"}));
+		JComboBox comboMoneda2 = new JComboBox<Object>();
+		comboMoneda2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				a = comboMoneda2.getSelectedItem().toString();
+			}
+
+			
+		});
+		comboMoneda2.setModel(new DefaultComboBoxModel(new String[] {"ARS", "USD", "EUR", "LIBRAS", "YEN", "WON"}));
 		comboMoneda2.setBounds(159, 118, 61, 22);
 		contenedorPrincipal.add(comboMoneda2);
 		
@@ -84,7 +102,7 @@ public class Gui extends JFrame {
 		lblTitulo = new JLabel("CONVERSOR DE MONEDA");
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTitulo.setBounds(29, 11, 305, 14);
+		lblTitulo.setBounds(0, 0, 356, 25);
 		contenedorPrincipal.add(lblTitulo);
 		
 		lblCoonversor1 = new JLabel("Introducir Cantidad:");
@@ -101,17 +119,17 @@ public class Gui extends JFrame {
 		btnConvertir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				double total;
 				
-				double conversor1 = Double.parseDouble(txtConversor1.getText());
 				
-				if(comboMoneda1.getSelectedItem().toString() == "ARG" && 
-						       comboMoneda2.getSelectedItem().toString() == "USD") {
-					
-					total = conversor1 * 100.00;
-					JOptionPane.showMessageDialog(null, "TOTAL DEL "+total);
-				}
 				
+				
+				convertir();
+				
+//				if (conversor1.isEmpty()) {
+//					JOptionPane.showMessageDialog(null, "EL CAMPO ESTA VACIO", "ERROR",0);
+//				}
+				
+//								
 				
 			}
 		});
@@ -122,19 +140,98 @@ public class Gui extends JFrame {
 		separator.setBounds(169, 195, 0, 2);
 		contenedorPrincipal.add(separator);
 		
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(0, 176, 356, 2);
-		contenedorPrincipal.add(separator_1);
+	}
+
+	public void convertir() {
+		cantidad = Double.parseDouble(txtConversor1.getText().trim());
+		switch(de) {
+		case "ARS":
+			if(a.equals("USD")) {
+				conversion = cantidad * 0.0049;
+			}else if(a.equals("EUR")) {
+				conversion = cantidad * 0.0046;
+			}else if(a.equals("LIBRAS")) {
+				conversion = cantidad * 0.0041;
+			}else if(a.equals("YEN")) {
+				conversion = cantidad * 0.66;
+			}else if(a.equals("WON")) {
+				conversion = cantidad * 6.45;
+			}
+			break;
+			
+		case "USD":
+			if(a.equals("ARS")) {
+				conversion = cantidad * 202.09;
+			}else if(a.equals("EUR")) {
+				conversion = cantidad * 0.93;
+			}else if(a.equals("LIBRAS")) {
+				conversion = cantidad * 0.82;
+			}else if(a.equals("YEN")) {
+				conversion = cantidad * 134.26;
+			}else if(a.equals("WON")) {
+				conversion = cantidad * 1302.53;
+			}
+			break;
+			
+		case "EUR":
+			if(a.equals("USD")) {
+				conversion = cantidad * 1.07;
+			}else if(a.equals("ARS")) {
+				conversion = cantidad * 216.86;
+			}else if(a.equals("LIBRAS")) {
+				conversion = cantidad * 0.88;
+			}else if(a.equals("YEN")) {
+				conversion = cantidad * 144.09;
+			}else if(a.equals("WON")) {
+				conversion = cantidad * 1397.67;
+			}
+			break;
+			
+		case "LIBRAS":
+			if(a.equals("USD")) {
+				conversion = cantidad * 1.22;
+			}else if(a.equals("EUR")) {
+				conversion = cantidad * 1.13;
+			}else if(a.equals("ARS")) {
+				conversion = cantidad * 245.60;
+			}else if(a.equals("YEN")) {
+				conversion = cantidad * 163.17;
+			}else if(a.equals("WON")) {
+				conversion = cantidad * 1582.81;
+			}
+			break;
+			
+			
+		case "YEN":
+			if(a.equals("USD")) {
+				conversion = cantidad * 0.0074;
+			}else if(a.equals("EUR")) {
+				conversion = cantidad * 0.0069;
+			}else if(a.equals("LIBRAS")) {
+				conversion = cantidad * 0.0061;
+			}else if(a.equals("ARS")) {
+				conversion = cantidad * 1.50;
+			}else if(a.equals("WON")) {
+				conversion = cantidad * 9.70;
+			}
+			break;
+			
+		case "WON":
+			if(a.equals("USD")) {
+				conversion = cantidad * 0.00077;
+			}else if(a.equals("EUR")) {
+				conversion = cantidad * 0.00071;
+			}else if(a.equals("LIBRAS")) {
+				conversion = cantidad * 0.00063;
+			}else if(a.equals("YEN")) {
+				conversion = cantidad * 0.10;
+			}else if(a.equals("ARS")) {
+				conversion = cantidad * 0.15;
+			}
+			break;
+		}
+		JOptionPane.showMessageDialog(null, "TOTAL CONVERSION "+ conversion);
 		
-		JLabel lblResultado = new JLabel("RESULTADO:");
-		lblResultado.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblResultado.setBounds(86, 184, 83, 28);
-		contenedorPrincipal.add(lblResultado);
-		
-		textField = new JTextField();
-		textField.setBounds(179, 189, 86, 20);
-		contenedorPrincipal.add(textField);
-		textField.setColumns(10);
 		
 	}
 }
